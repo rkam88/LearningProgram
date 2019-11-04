c856d8db660a08e221144521f031321d3030efeb
Roman Kamyshnikov <rkam88@gmail.com>
2019-11-04 10:34:16 +0300

    change main activity to use lecture list fragment

---
+++ app/src/main/java/net/rusnet/sb/learningprogram/LectureListFragment.java
@@ -0,0 +1,204 @@
+package net.rusnet.sb.learningprogram;
+
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import net.rusnet.sb.learningprogram.adapters.LearningProgramAdapter;
+import net.rusnet.sb.learningprogram.adapters.LectorSpinnerAdapter;
+import net.rusnet.sb.learningprogram.dataprovider.LearningProgramProvider;
+import net.rusnet.sb.learningprogram.models.Lecture;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LectureListFragment extends Fragment {
+
+    private static final int POSITION_ALL = 0;
+
+    private RecyclerView mRecyclerView;
+    private Spinner mWeekSpinner;
+    private Spinner mSpinner;
+    private LinearLayoutManager mLinearLayoutManager;
+    private LearningProgramProvider mLearningProgramProvider = new LearningProgramProvider();
+    private LearningProgramAdapter mAdapter;
+
+    private boolean mShowWeeks;
+    private boolean mFilterByLector;
+    private String mLectorName;
+
+    private Resources mResources;
+
+    public static Fragment newInstance() {
+        return new LectureListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_lecture_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mWeekSpinner = view.findViewById(R.id.week_selection_spinner);
+        mSpinner = view.findViewById(R.id.lectors_spinner);
+        mRecyclerView = view.findViewById(R.id.learning_program_recycler);
+
+        initRecyclerView();
+        mResources = getResources();
+        new LoadLecturesFromWebTask(this).execute();
+    }
+
+    private void initRecyclerView() {
+
+        mLinearLayoutManager = new LinearLayoutManager(
+                mRecyclerView.getContext(),
+                RecyclerView.VERTICAL,
+                false
+        );
+        mRecyclerView.setLayoutManager(mLinearLayoutManager); //можно задать и в .xml
+        mAdapter = new LearningProgramAdapter();
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration divider = new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(divider);
+    }
+
+    private void updateAdapter() {
+        mAdapter.setLectures(mResources, mLearningProgramProvider.provideLectures(), mShowWeeks);
+    }
+
+    private void initSpinner() {
+
+        List<String> lectors = mLearningProgramProvider.provideLectors();
+        Collections.sort(lectors);
+        lectors.add(POSITION_ALL, getResources().getString(R.string.all));
+
+
+        final LectorSpinnerAdapter spinnerAdapter = new LectorSpinnerAdapter(lectors);
+        mSpinner.setAdapter(spinnerAdapter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mLectorName = "";
+                List<Lecture> filteredLectures;
+                if (position == POSITION_ALL) {
+                    filteredLectures = mLearningProgramProvider.provideLectures();
+                    mFilterByLector = false;
+                } else {
+                    mLectorName = spinnerAdapter.getItem(position);
+                    filteredLectures = mLearningProgramProvider.filterBy(mLectorName);
+                    mFilterByLector = true;
+                }
+                mAdapter.setLectures(mResources, filteredLectures, mShowWeeks);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        int spinnerPosition = mSpinner.getSelectedItemPosition();
+        mFilterByLector = spinnerPosition != POSITION_ALL;
+
+    }
+
+    private void initWeekSelectionSpinner() {
+
+        final List<String> weekSelectionSpinnerItems = new ArrayList<>();
+        weekSelectionSpinnerItems.add(getResources().getString(R.string.hide_weeks));
+        weekSelectionSpinnerItems.add(getResources().getString(R.string.show_weeks));
+
+        ArrayAdapter weekAdapter = new ArrayAdapter<String>(
+                mWeekSpinner.getContext(),
+                android.R.layout.simple_spinner_item,
+                weekSelectionSpinnerItems);
+
+        mWeekSpinner.setAdapter(weekAdapter);
+
+        mWeekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String spinnerValue = mWeekSpinner.getSelectedItem().toString();
+                if (spinnerValue.equals(getResources().getString(R.string.hide_weeks))) {
+                    mShowWeeks = false;
+                } else {
+                    mShowWeeks = true;
+                }
+
+                if (!mFilterByLector) {
+                    mAdapter.setLectures(mResources, mLearningProgramProvider.provideLectures(), mShowWeeks);
+                    mAdapter.notifyDataSetChanged();
+                } else {
+                    mAdapter.setLectures(mResources, mLearningProgramProvider.filterBy(mLectorName), mShowWeeks);
+                    mAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        String spinnerValue = mWeekSpinner.getSelectedItem().toString();
+        if (spinnerValue.equals(getResources().getString(R.string.hide_weeks))) {
+            mShowWeeks = false;
+        } else {
+            mShowWeeks = true;
+        }
+
+    }
+
+    private static class LoadLecturesFromWebTask extends AsyncTask<Void, Void, List<Lecture>> {
+        private final WeakReference<LectureListFragment> mFragmentRef;
+        private final LearningProgramProvider mProvider;
+
+        private LoadLecturesFromWebTask(LectureListFragment fragment) {
+            mFragmentRef = new WeakReference<>(fragment);
+            mProvider = fragment.mLearningProgramProvider;
+        }
+
+        @Override
+        protected List<Lecture> doInBackground(Void... voids) {
+            return mProvider.downloadLectures();
+        }
+
+        @Override
+        protected void onPostExecute(List<Lecture> lectures) {
+            LectureListFragment fragment = mFragmentRef.get();
+            if (lectures == null) {
+                Toast.makeText(fragment.getContext(), R.string.loading_data_failed, Toast.LENGTH_SHORT).show();
+            } else {
+                fragment.initSpinner();
+                fragment.initWeekSelectionSpinner();
+                fragment.updateAdapter();
+            }
+        }
+    }
+}
--- app/src/main/java/net/rusnet/sb/learningprogram/MainActivity.java
+++ app/src/main/java/net/rusnet/sb/learningprogram/MainActivity.java
@@ -1,187 +1,23 @@
 package net.rusnet.sb.learningprogram;
 
-import android.content.res.Resources;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import net.rusnet.sb.learningprogram.adapters.LearningProgramAdapter;
-import net.rusnet.sb.learningprogram.adapters.LectorSpinnerAdapter;
-import net.rusnet.sb.learningprogram.dataprovider.LearningProgramProvider;
-import net.rusnet.sb.learningprogram.models.Lecture;
-
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int POSITION_ALL = 0;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager layoutManager;
-    private LearningProgramProvider mLearningProgramProvider = new LearningProgramProvider();
-    private LearningProgramAdapter adapter;
-
-    private boolean showWeeks;
-    private boolean filterByLector;
-    private String lectorName;
-
-    private Resources mResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initRecyclerView();
-
-        mResources = getResources();
-
-        new LoadLecturesFromWebTask(this).execute();
-
-    }
-
-    private void initRecyclerView() {
-        recyclerView = findViewById(R.id.learning_program_recycler);
-        layoutManager = new LinearLayoutManager(
-                this,
-                RecyclerView.VERTICAL,
-                false
-        );
-        recyclerView.setLayoutManager(layoutManager); //можно задать и в .xml
-        adapter = new LearningProgramAdapter();
-
-        recyclerView.setAdapter(adapter);
-
-        DividerItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(divider);
-    }
-
-    private void updateAdapter() {
-        adapter.setLectures(mResources, mLearningProgramProvider.provideLectures(), showWeeks);
-    }
-
-    private void initSpinner() {
-        Spinner spinner = findViewById(R.id.lectors_spinner);
-        List<String> lectors = mLearningProgramProvider.provideLectors();
-        Collections.sort(lectors);
-        lectors.add(POSITION_ALL, getResources().getString(R.string.all));
-
-
-        final LectorSpinnerAdapter spinnerAdapter = new LectorSpinnerAdapter(lectors);
-        spinner.setAdapter(spinnerAdapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                lectorName = "";
-                List<Lecture> filteredLectures;
-                if (position == POSITION_ALL) {
-                    filteredLectures = mLearningProgramProvider.provideLectures();
-                    filterByLector = false;
-                } else {
-                    lectorName = spinnerAdapter.getItem(position);
-                    filteredLectures = mLearningProgramProvider.filterBy(lectorName);
-                    filterByLector = true;
-                }
-                adapter.setLectures(mResources, filteredLectures, showWeeks);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        int spinnerPosition = spinner.getSelectedItemPosition();
-        filterByLector = spinnerPosition != POSITION_ALL;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_placeholder, LectureListFragment.newInstance())
+                .commit();
 
     }
 
-    private void initWeekSelectionSpinner() {
-        final Spinner weekSpinner = findViewById(R.id.week_selection_spinner);
-        final List<String> weekSelectionSpinnerItems = new ArrayList<>();
-        weekSelectionSpinnerItems.add(getResources().getString(R.string.hide_weeks));
-        weekSelectionSpinnerItems.add(getResources().getString(R.string.show_weeks));
 
-        ArrayAdapter weekAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_spinner_item,
-                weekSelectionSpinnerItems);
-
-        weekSpinner.setAdapter(weekAdapter);
-
-        weekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                String spinnerValue = weekSpinner.getSelectedItem().toString();
-                if (spinnerValue.equals(getResources().getString(R.string.hide_weeks))) {
-                    showWeeks = false;
-                } else {
-                    showWeeks = true;
-                }
-
-                if (!filterByLector) {
-                    adapter.setLectures(mResources, mLearningProgramProvider.provideLectures(), showWeeks);
-                    adapter.notifyDataSetChanged();
-                } else {
-                    adapter.setLectures(mResources, mLearningProgramProvider.filterBy(lectorName), showWeeks);
-                    adapter.notifyDataSetChanged();
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        String spinnerValue = weekSpinner.getSelectedItem().toString();
-        if (spinnerValue.equals(getResources().getString(R.string.hide_weeks))) {
-            showWeeks = false;
-        } else {
-            showWeeks = true;
-        }
-
-    }
-
-    private static class LoadLecturesFromWebTask extends AsyncTask<Void, Void, List<Lecture>> {
-        private final WeakReference<MainActivity> mActivityRef;
-        private final LearningProgramProvider mProvider;
-
-        private LoadLecturesFromWebTask(MainActivity activity) {
-            mActivityRef = new WeakReference<>(activity);
-            mProvider = activity.mLearningProgramProvider;
-        }
-
-        @Override
-        protected List<Lecture> doInBackground(Void... voids) {
-            return mProvider.downloadLectures();
-        }
-
-        @Override
-        protected void onPostExecute(List<Lecture> lectures) {
-            MainActivity activity = mActivityRef.get();
-            if (lectures == null) {
-                Toast.makeText(activity, R.string.loading_data_failed, Toast.LENGTH_SHORT).show();
-            } else {
-                activity.initSpinner();
-                activity.initWeekSelectionSpinner();
-                activity.updateAdapter();
-            }
-        }
-    }
 }
